286e990cc0b0b1096bb848cbb862bd3bddfce4c0
Mateusz Klosinski <matix003@gmail.com>
2020-04-14 17:48:00 +0200

    Added project name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Class<?> clazz = Main.class;
+        Package pacage = clazz.getPackage();
+
+        System.out.println("Nazwa projektu");
+        System.out.println(pacage.getName());
+
         System.out.println("JDK");
         System.out.println(System.getProperty("java.version"));
 
