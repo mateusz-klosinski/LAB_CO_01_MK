58d2bfd35a42b91ee21f519de934d1ffbd36e7b3
Mateusz Klosinski <matix003@gmail.com>
2020-04-14 17:22:59 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("JDK");
+        System.out.println(System.getProperty("java.version"));
+
+        System.out.println("JRE");
+        System.out.println(System.getProperty("java.runtime.version"));
+    }
+}
